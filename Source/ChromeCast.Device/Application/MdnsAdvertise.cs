43d085aa8e7bf83f30510b2218d5fa5902fe2710
SamDel <github@deaut.nl>
2021-01-25 17:35:44 +0100

    Mdns changes

--- Source/ChromeCast.Device/Application/MdnsAdvertise.cs
+++ Source/ChromeCast.Device/Application/MdnsAdvertise.cs
@@ -1,7 +1,9 @@
-﻿using ChromeCast.Classes;
-using ChromeCast.Device.Log.Interfaces;
+﻿using ChromeCast.Device.Log.Interfaces;
 using Makaretu.Dns;
 using System;
+using System.Linq;
+using System.Net.Sockets;
+using Tmds.MDns;
 
 namespace ChromeCast.Device.Application
 {
@@ -10,7 +12,9 @@ namespace ChromeCast.Device.Application
         private readonly ILogger logger;
         private readonly string deviceName;
         private readonly string serviceType = "_googlecast._tcp";
+        private const string serviceTypeEmbedded = "_googlezone._tcp";
         private readonly ushort port = 8009;
+        private MulticastService mdns;
 
         public MdnsAdvertise(ILogger loggerIn, string deviceNameIn)
         {
@@ -20,9 +24,15 @@ namespace ChromeCast.Device.Application
 
         public void Advertise()
         {
-            var mdns = new MulticastService();
-            var sd = new ServiceDiscovery(mdns);
-            var instanceName = SystemCalls.SystemGuid();
+            mdns = new MulticastService();
+            mdns.QueryReceived += Mdns_QueryReceived;
+            mdns.AnswerReceived += Mdns_AnswerReceived;
+
+            var serviceDiscovery = new ServiceDiscovery(mdns);
+            serviceDiscovery.ServiceDiscovered += ServiceDiscovery_ServiceDiscovered;
+            serviceDiscovery.ServiceInstanceDiscovered += ServiceDiscovery_ServiceInstanceDiscovered;
+
+            var instanceName = string.Empty;// SystemCalls.SystemGuid();
             var serviceProfile = new ServiceProfile(instanceName, serviceType, port)
             {
                 InstanceName = $"{deviceName}-{instanceName.Replace("-", "")}"
@@ -33,16 +43,72 @@ namespace ChromeCast.Device.Application
             serviceProfile.AddProperty("ve", "00");
             serviceProfile.AddProperty("md", "SamDel");
             serviceProfile.AddProperty("ic", "");
-            serviceProfile.AddProperty("fn", deviceName);
+            serviceProfile.AddProperty("fn", string.Empty); // deviceName);
             serviceProfile.AddProperty("ca", "0000");
             serviceProfile.AddProperty("st", "0");
             serviceProfile.AddProperty("bs", "000000000000");
             serviceProfile.AddProperty("nf", "0");
             serviceProfile.AddProperty("rs", "");
-            sd.Advertise(serviceProfile);
+            serviceDiscovery.Advertise(serviceProfile);
 
             mdns.Start();
             logger.Log($"MdnsAdvertise: Advertising {instanceName}-{serviceType}-{port}...");
+
+            ServiceBrowser serviceBrowser = new ServiceBrowser();
+            serviceBrowser.ServiceAdded += OnServiceAdded;
+            serviceBrowser.ServiceRemoved += OnServiceRemoved;
+            serviceBrowser.ServiceChanged += OnServiceChanged;
+            serviceBrowser.StartBrowse(serviceType);
+
+            ServiceBrowser serviceBrowserEmbedded = new ServiceBrowser();
+            serviceBrowserEmbedded.ServiceAdded += OnServiceAdded;
+            serviceBrowserEmbedded.ServiceRemoved += OnServiceRemoved;
+            serviceBrowserEmbedded.ServiceChanged += OnServiceChanged;
+            serviceBrowserEmbedded.StartBrowse(serviceTypeEmbedded);
+        }
+
+        private void Mdns_QueryReceived(object sender, MessageEventArgs e)
+        {
+            var msg = e.Message;
+            if (msg.Questions.Any(q => q.Name.ToString().Contains(serviceType)))
+            {
+                var res = msg.CreateResponse();
+                var addresses = MulticastService.GetIPAddresses()
+                    .Where(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+                foreach (var address in addresses)
+                {
+                    res.Answers.Add(new ARecord
+                    {
+                        Name = serviceType,
+                        Address = address
+                    });
+                }
+                mdns.SendAnswer(res);
+            }
+        }
+
+        private void OnServiceChanged(object sender, ServiceAnnouncementEventArgs e)
+        {
+        }
+
+        private void OnServiceRemoved(object sender, ServiceAnnouncementEventArgs e)
+        {
+        }
+
+        private void OnServiceAdded(object sender, ServiceAnnouncementEventArgs e)
+        {
+        }
+
+        private void ServiceDiscovery_ServiceInstanceDiscovered(object sender, ServiceInstanceDiscoveryEventArgs e)
+        {
+        }
+
+        private void ServiceDiscovery_ServiceDiscovered(object sender, DomainName e)
+        {
+        }
+
+        private void Mdns_AnswerReceived(object sender, MessageEventArgs e)
+        {
         }
     }
 }
