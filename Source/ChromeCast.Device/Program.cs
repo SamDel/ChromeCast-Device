377f5f22bdfc99abcd76477897e8394333d0a87a
SamDel <github@deaut.nl>
2021-03-27 16:31:46 +0100

    Fix for reading the device name from the arguments.

--- Source/ChromeCast.Device/Program.cs
+++ Source/ChromeCast.Device/Program.cs
@@ -9,7 +9,7 @@ namespace ChromeCast
             var log = args.Any(x => x == "-l");
             var deviceName = "Device";
             var index = args.Select((s, i) => new { i, s }).Where(t => t.s == "-n").Select(t => t.i).ToList().FirstOrDefault();
-            if (index > 0 && args.Length >= index + 1)
+            if (index >= 0 && args.Length >= index + 1)
                 deviceName = args[index + 1];
 
             _ = new Device.Application.Device(log, deviceName);
